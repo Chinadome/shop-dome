884a1df549573425a4f4f80bd7bf3a7484fa5fd6
unknown <884159021@qq.com>
2020-01-08 23:46:23 +0800

    修改代码

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -4,16 +4,16 @@
          <div class="top_container">
              <img src="../assets/images/kant.png">
          </div>
-         <el-form label-width="0px" class="login_form">
-                <el-form-item>
-                    <el-input prefix-icon="el-icon-user-solid"></el-input>
+         <el-form ref="Loginfrom" :model="frommodel" :rules="rules" label-width="0px" class="login_form">
+                <el-form-item  prop="username">
+                    <el-input v-model='frommodel.username' prefix-icon="el-icon-user-solid"></el-input>
                 </el-form-item>
-                <el-form-item>
-                    <el-input prefix-icon="el-icon-search"></el-input>
+                <el-form-item  prop="password">
+                    <el-input v-model='frommodel.password' prefix-icon="el-icon-search"></el-input>
                 </el-form-item>
                 <el-form-item class="btns">
-                    <el-button type="primary" plain>登录</el-button>
-                    <el-button type="info" plain>重置</el-button>
+                    <el-button type="primary" plain @click="login()">登录</el-button>
+                    <el-button type="info" plain @click="reset()">重置</el-button>
                 </el-form-item>
             </el-form>
      </div>
@@ -21,6 +21,54 @@
 </template>
 <script>
 export default {
+    data(){
+        return{
+             frommodel: {
+             username: 'cheshi',
+             password: '123456'
+             },
+             rules:{
+                 username: [{required: true, message: '账号不可为空', trigger: 'blur'
+                 },{
+                 min: 3, max: 6, message: '账号长度在 3 到 6 个字符', trigger: 'blur' 
+                 }],
+                password:[{
+                required: true, message: '密码不可为空', trigger: 'blur'
+                },{
+                min: 6, max: 16, message: '密码长度在 6 到 16个字符', trigger: 'blur' 
+                }]
+             }
+        }
+    },
+     methods: {
+    reset() {
+      this.$refs.Loginfrom.resetFields()
+    },
+    login() {
+      this.$refs.Loginfrom.validate((valid) => {
+        console.log(this.frommodel.password);
+         if (!valid) {
+         return this.$router.push("/login");}
+         if(this.frommodel.username=="cheshi"&&this.frommodel.password=="123456")
+         return  this.$message.success('登录成功')
+        else
+        return  this.$message.error('登录失败')
+      })
+    }
+    // Login(){
+    //     this.$refs.Loginfrom.validate(async (valid)=>{
+    //        if(!valid)
+    //        return this.$router.push('/login');
+    //        const {data:res}=await this.$http.post('/login',this.Loginfrom);
+    //         console.log(res);
+    //     //    if(res.meta.stauts!==200)
+    //     //    this.$message.error("登录失败");
+    //     //    else
+    //     //    this.$message.success("登录成功");
+
+    //     })
+    // }
+  }
 }
 </script>
 <style lang="less" scoped>
