6d6f00bdf94286248db0503550a93816f6d1e1eb
unknown <884159021@qq.com>
2020-02-15 21:35:11 +0800

    添加权限

--- vue-shop/src/components/power/Roles.vue
+++ vue-shop/src/components/power/Roles.vue
@@ -11,6 +11,32 @@
             </el-col>
             </el-row>
              <el-table :data='rolesList' style="width: 100%" border stripe>
+              <el-table-column type="expand">
+      <template slot-scope="scope">
+         <el-row v-for="(item1,index1) in scope.row.children" :key="item1.id" :class="['v-tag-center','bottom',index1===0 ? 'btop':''] ">
+              <el-col :span="5" >
+                <el-tag closable @close="removeById(scope.row,item1.id)">{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <el-col :span="19">
+                <!--第二层-->
+                <el-row v-for="(item2,index2) in item1.children" :key="item2.id" :class="['v-tag-center','bottom',index2===0 ? 'btop':''] ">
+                    <el-col :spam="6">
+                      <el-tag closable type="success" @close="removeById(scope.row,item2.id)">
+                        {{item2.authName}}
+                        <i class="el-icon-caret-right"></i>
+                      </el-tag>
+                    </el-col>
+                    <el-col :spam="18">
+                      <el-tag v-for="(item3) in item2.children" :key="item3.id" closable type="warning" @close="removeById(scope.row,item3.id)">
+                        {{item3.authName}}
+                      </el-tag>
+                    </el-col>
+                </el-row>
+              </el-col>
+          </el-row>
+      </template>
+    </el-table-column>
             <el-table-column  align="center" type="index" label="序号" width="60px">
            </el-table-column>
            <el-table-column prop="roleName" align="center" label="角色名称">
@@ -22,7 +48,7 @@
               <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)">编辑
               </el-button>
               <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeRolesById(scope.row.id)">删除</el-button>
-              <el-button type="warning" icon="el-icon-setting" size="mini">分配角色</el-button>
+              <el-button type="warning" icon="el-icon-setting" size="mini" @click="rolesTreeDialog(scope.row)">分配权限</el-button>
               </template>
            </el-table-column>
          </el-table>
@@ -60,6 +86,25 @@
           <span slot="footer" class="dialog-footer">
          <el-button @click="editRolesdialog = false">取 消</el-button>
          <el-button type="primary" @click="editRolesList">确 定</el-button>
+          </span>
+          </el-dialog>
+             <!--分配角色-->
+          <el-dialog
+         title="权限分配"
+        :visible.sync="treedialog"
+        width="70%">
+        <el-tree
+      ref="tree"
+      :data="treeList"
+      show-checkbox
+       node-key="id"
+     :default-expanded-keys="dialogRolesKyes"
+     :default-checked-keys="dialogKeys"
+      :props="defaultProps">
+   </el-tree>
+        <span slot="footer" class="dialog-footer">
+         <el-button @click="treedialog = false;roleId = ''">取 消</el-button>
+         <el-button type="primary" @click="addTree">确 定</el-button>
           </span>
           </el-dialog>
           </el-card>
@@ -101,7 +146,18 @@ export default {
           { required: false, message: '请输入角色描述', trigger: 'blur' },
           { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
         ]
-      }
+      },
+      treedialog: false,
+      treeList: [],
+      dialogKeys: [],
+      dialogRolesKyes: [],
+      defaultProps: {
+        children: 'children',
+        label: 'authName',
+        disabled: false,
+        isLeaf: true
+      },
+      roleId: ''
     }
   },
   created () {
@@ -121,7 +177,7 @@ export default {
     addDialogRoles () {
       this.$refs.addRolesRefs.resetFields()
       // 为了防止resetFields() 失效
-      this.addRoles = []
+      this.addRoles = {}
     },
     // 增加角色
     addRolesList () {
@@ -175,10 +231,79 @@ export default {
         this.getRolesList()
         this.$message.success('更新列表成功！')
       })
+    },
+    // 根据角色删除权限
+    async  removeById (item, id) {
+      const confirm = await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (confirm !== 'confirm') {
+        return this.$message.info('删除已取消')
+      }
+      const { data: res } = await this.$http.delete(`roles/${item.id}/rights/${id}`)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除失败！')
+      }
+      this.$message.success('删除成功')
+      item.children = res.data
+    },
+    async rolesTreeDialog (items) {
+      this.roleId = ''
+      this.treedialog = true
+      this.dialogRolesKyes = []
+      this.dialogKeys = []
+      const { data: res } = await this.$http.get('rights/tree')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取权限列表失败！')
+      }
+      this.treeList = res.data
+      this.roleId = items.id
+      const item = items.children
+      for (let i = 0; i < item.length; i++) {
+        const children1 = []
+        for (let j = 0; j < item[i].children.length; j++) {
+          const children2 = []
+          children1.push(children2)
+          for (let z = 0; z < item[i].children[j].children.length; z++) {
+            this.dialogKeys.push(item[i].children[j].children[z].id)
+            children2.push(item[i].children[j].children[z].id)
+          }
+          children1.push(item[i].children[j].id)
+          children1.push(children2)
+        }
+        this.dialogRolesKyes.push(item[i].id)
+        this.dialogRolesKyes.push(children1)
+      }
+    },
+    async  addTree () {
+      alert(this.roleId + '***' + this.$refs.tree.getCheckedKeys())
+      const { data: res } = await this.$http.post(`roles/${this.roleId}/rights`,
+        {
+          roleId: this.roleId,
+          rids: this.$refs.tree.getCheckedKeys()
+        })
+      if (res.meta.status !== 201) return this.$message.error('添加权限失败')
+      this.$message.success('添加成功')
+      this.treedialog = false
+      this.getRolesList()
     }
   }
 }
 </script>
 <style lang="less">
-
+.el-tag{
+  margin: 8px;
+}
+.btop{
+ border-top: 1px solid #eee;
+}
+.bottom{
+ border-bottom: 1px solid  #eee ;
+}
+.v-tag-center{
+  display: flex;
+  align-items: center;
+}
 </style>
